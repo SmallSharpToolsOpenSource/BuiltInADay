f0004fdc58002ef3c7d401c32f6a74eb8ba3f824
Brennan Stehling <brennan@smallsharptools.com>
2015-05-28 16:11:55 -0700

    automated loop over pods

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-pod install --no-integrate
-
-# fetching a list of the project names from the Podfile would be helpful here
-
-./build_universal.sh Alamofire
-./build_universal.sh AFNetworking
-./build_universal.sh FormatterKit
-
+GREEN_COLOR="\033[0;32m"
 BLUE_COLOR='\033[0;34m'
 NO_COLOR='\033[0m'
+
+pod install --no-integrate
+
+PODS=`cat Podfile | grep "^[\t ]*pod" | sed s/"^[\t ]*pod ['\"]"// | sed s/"['\"].*$"// | uniq`
+
+for p in $PODS
+do
+  echo "${BLUE_COLOR}=== Building ===${p}${NO_COLOR}"
+  ./build_universal.sh "${p}"
+done
+
 echo "${BLUE_COLOR}Done${NO_COLOR}"
